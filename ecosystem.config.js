847c95104ce760a71ea97782f16c5ae586d3ac7e
Sunro1994 <gwangjulsr@gmail.com>
2026-05-04 23:16:56 +0900

    chore: PM2 ecosystem 설정 파일 추가

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'resume-helper',
+      script: 'node_modules/.bin/next',
+      args: 'start',
+      cwd: '/home/ubuntu/app',
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+      },
+    },
+  ],
+};
